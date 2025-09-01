9ad4ed5ffdf29faddd6d773b37c8500b73be19b3
Badmus2005 <tchidehoudodjivirgile@gmail.com>
2025-09-01 18:11:15 +0100

    Changement

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', async function () {
 
     // Charger le profil utilisateur
     try {
-        window.userProfile = await fetchWithAuth(CONFIG.API.ENDPOINTS.USERS.PROFILE);
+        window.userProfile = await fetchWithAuth(CONFIG.API.BASE_URL.ENDPOINTS.USERS.PROFILE);
         updateUIWithUserProfile();
     } catch (error) {
         console.error('Erreur lors du chargement du profil:', error);
